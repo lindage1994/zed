6a2622cbcd30a063c7322ce9e32cdb746c6fea85
DESKTOP-3M8EM7B\zed <iahsnil1994@gmail.com>
2019-10-23 14:28:24 +0700

    concurrent utils and LRU

---
+++ src/main/java/com/example/zed/concurrent/CountDownLatchTest.java
@@ -0,0 +1,47 @@
+package com.example.zed.concurrent;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @Author: zed
+ * @Date: 2019/10/8 10:33
+ * @Description: CountDownLatch
+ */
+public class CountDownLatchTest {
+
+    public static void main(String[] args) {
+        final CountDownLatch latch = new CountDownLatch(3);
+
+        new Thread(() -> {
+            try {
+                System.out.println("子线程" + Thread.currentThread().getName() + "正在执行");
+                Thread.sleep(3000);
+                System.out.println("子线程" + Thread.currentThread().getName() + "执行完毕");
+                latch.countDown();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                System.out.println("子线程" + Thread.currentThread().getName() + "正在执行");
+                Thread.sleep(4000);
+                System.out.println("子线程" + Thread.currentThread().getName() + "执行完毕");
+                latch.countDown();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        try {
+            System.out.println("等待两个子线程执行完毕...");
+            latch.await();
+            System.out.println("2个子线程已经执行完毕");
+            System.out.println("继续执行主线程");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/main/java/com/example/zed/concurrent/CyclicBarrierTest.java
@@ -0,0 +1,49 @@
+package com.example.zed.concurrent;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * @Author: zed
+ * @Date: 2019/10/8 10:52
+ * @Description: CyclicBarrierTest
+ */
+public class CyclicBarrierTest {
+    public static void main(String[] args) {
+        int N = 4;
+        CyclicBarrier barrier  = new CyclicBarrier(N, ()-> System.out.println("写入数据执行完毕... 执行线程：" + Thread.currentThread().getName()));
+        for(int i=0;i<N;i++) {
+            new Writer(barrier).start();
+        }
+        try {
+            Thread.sleep(25000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("CyclicBarrier重用");
+
+        for(int i=0;i<N;i++) {
+            new Writer(barrier).start();
+        }
+    }
+    static class Writer extends Thread{
+        private CyclicBarrier cyclicBarrier;
+        Writer(CyclicBarrier cyclicBarrier) {
+            this.cyclicBarrier = cyclicBarrier;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("线程"+Thread.currentThread().getName()+"正在写入数据...");
+            try {
+                Thread.sleep(5000);      //以睡眠来模拟写入数据操作
+                System.out.println("线程"+Thread.currentThread().getName()+"写入数据完毕，等待其他线程写入完毕");
+                cyclicBarrier.await();
+            } catch (InterruptedException | BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+            System.out.println("所有线程写入完毕，继续处理其他任务...");
+        }
+    }
+}
